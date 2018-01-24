56491d223f9834fc461cb067fba5c1270e51c4a9
fuziqian <ziqianfu@usc.edu>
2018-01-24 12:04:09 -0800

    version 2.0

--- warmup1.c
+++ warmup1.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include "my402list.h"
 
-
 typedef struct transaction {
-	long timestamp;
-	char* description;
+	time_t timestamp;
+	char description[25];
 	long amount;
 	int isAdd;
 } MyTransaction;
@@ -15,8 +15,8 @@ typedef struct transaction {
 My402ListElem* CompareAndInsert(My402List* plist, My402ListElem* toInsert){
 	My402ListElem* cur = My402ListPrev(plist, toInsert);
 	My402ListElem* elemNext = My402ListNext(plist, toInsert);
-	My402ListUnlink(plist, toInsert);
 	MyTransaction* transToInsert = (MyTransaction*)toInsert->obj;
+	My402ListUnlink(plist, toInsert);
 	free(toInsert);
 	while(cur!=NULL){
 		MyTransaction* curTrans = (MyTransaction*)cur->obj;
@@ -36,12 +36,44 @@ My402ListElem* CompareAndInsert(My402List* plist, My402ListElem* toInsert){
 	return elemNext;
 }
 
-long StringToLong(char* num){
+long AmountStringToLong(char* num){
 	int len = strlen(num), i;
+	int valid = 1;
 	long res = 0;
 	for(i=0;i<len;i++){
-		if(num[i] == '.') continue;
+		if(num[i] == '.'){
+			if(i == len - 3 && i != 0) continue;
+			else{
+				valid = 0;
+				break;
+			}
+		}
+		if(!isdigit(num[i])){
+			valid = 0;
+			break;
+		}
 		res = res*10 + (num[i] - '0');
+	}
+	if(!valid){
+		fprintf(stderr, "%s\n", "Format error with amount");
+		exit(-1);
+	}
+	return res;
+}
+
+time_t TimestampStringToTimeT(char* timestamp){
+	int i;
+	if(strlen(timestamp)>10){
+		fprintf(stderr, "%s\n", "Timestamp input is too long!");
+		exit(-1);
+	}
+	time_t res = 0;
+	for(i=0;i<strlen(timestamp);i++){
+		if(!isdigit(timestamp[i])){
+			fprintf(stderr, "%s\n", "Timestamp input contains non-digit elements!");
+			exit(-1);
+		}
+		res = res*10 + (timestamp[i] - '0');
 	}
 	return res;
 }
@@ -52,14 +84,16 @@ void InsertSort(My402List* plist){
 	My402ListElem* nextNode = My402ListFirst(plist);
 	for(i=0;i<num_items;i++){
 		if(nextNode == NULL){
-			printf("%s\n", "LinedList Problem!");
+			fprintf(stderr, "%s\n", "LinedList's length is shorter than expected!");
 		}
 		nextNode = CompareAndInsert(plist, nextNode);
 	}
 }
 
-char* buildAmount(long amount, int isAdd){
-	char printAmount[15] = "              ";
+char* formatNum(long amount, int isAdd){
+	char *printAmount = (char*)malloc(15);
+	memset(printAmount, ' ', 14);
+	printAmount[14] = '\0';
 	int i;
 	printAmount[12] = '0';
 	printAmount[11] = '0';
@@ -69,6 +103,7 @@ char* buildAmount(long amount, int isAdd){
 		printAmount[0] = '(';
 		printAmount[13] = ')';
 	}
+
 	if(amount>=10000000){
 		for(i=12;i>=4;i--){
 			if(i==10) continue;
@@ -77,13 +112,39 @@ char* buildAmount(long amount, int isAdd){
 		return printAmount;
 	}
 	for(i=12;i>=4;i--){
-		if(amount==0) break;
-		if(i==10) continue;
+		if(amount == 0) break;
+		if(i == 10) continue;
 		char digit = '0' + amount%10;
 		printAmount[i] = digit; 
 		amount = amount/10;
 	}
 	return printAmount;
+}
+
+char* formatDate(time_t timestamp){
+	char *res = (char*)malloc(16);
+	char* dateString  = ctime(&timestamp);
+	if(dateString != NULL){
+		dateString[strlen(dateString) - 1] = '\0';
+	}
+	//char date[11];
+	strncpy(res, dateString, 11);
+	char year[5];
+	strncpy(year, dateString + 20, 4);
+	strcat(res, year);
+	res[15] = '\0';
+	return res;
+}
+
+char* formatDesc(char* desc){
+	int i;
+	char* res = (char*)malloc(25);
+	strncpy(res, desc, 24);
+	for(i = strlen(res);i<24;i++){
+		res[i] = ' ';
+	}
+	res[24] = '\0';
+	return res;
 }
 
 void sort(char *tfile){
@@ -97,37 +158,47 @@ void sort(char *tfile){
 	}
 	
 	if(fp==NULL){
-		printf("%s\n", "cannot find file.");
+		fprintf(stderr, "%s\n", "cannot find file.");
+		exit(-1);
 		return;
 	}
 	
-	printf("%s", "file opened\n");	
-	
 	My402List* trans = (My402List*)malloc(sizeof(My402List));
+	if(trans == NULL){
+		fprintf(stderr, "%s\n", "malloc linkedlist failed.");
+		exit(-1);
+	}
 	(void)My402ListInit(trans);
 
 	char line[1024];
 	while(fgets(line, len, fp) != NULL){
-		printf("%s\n", line);
 		size_t size = sizeof(line);
 		if(size>1024){
-			exit(1);
+			exit(-1);
 		}
 		char *signal = strtok(line, "\t");
 		char *timestamp = strtok(NULL, "\t");
 		char *amount = strtok(NULL, "\t");
 		char *desc = strtok(NULL, "\t");
-		//printf("%s\n", "finish split");
 		MyTransaction* newTrans = (MyTransaction*)malloc(sizeof(MyTransaction));
+		if(signal == NULL || strlen(signal) != 1){
+			fprintf(stderr, "%s\n", "The length sign in input is invalid!");
+			exit(-1);
+		}
 		if(signal[0]=='+'){
 			newTrans->isAdd = TRUE;
 		}
-		else{
+		else if(signal[0] == '-'){
 			newTrans->isAdd = FALSE;
 		}
-		newTrans->timestamp = atol(timestamp);
-		newTrans->amount = StringToLong(amount);
-		newTrans->description = desc;
+		else{
+			fprintf(stderr, "%s\n", "The sign in input is invalid!");
+			exit(-1);
+		}
+		newTrans->timestamp = TimestampStringToTimeT(timestamp);
+		newTrans->amount = AmountStringToLong(amount);
+		desc[strlen(desc)-1] = '\0';
+		strncpy(newTrans->description, desc, 24);
 		(void)My402ListAppend(trans, newTrans);
 	}
 	InsertSort(trans);
@@ -149,40 +220,37 @@ void sort(char *tfile){
 			
 			balance -= p->amount;
 		}
-		time_t date = (time_t)p->timestamp;
-		char* dateString  = ctime(&date);
-		if(dateString != NULL){
-			dateString[strlen(dateString) - 1] = '\0';
-		}
+		char* dateString = formatDate(p->timestamp);
+
 		strcat(line, dateString);
 		strcat(line, " | ");
-		char* amountString = buildAmount(p->amount, p->isAdd);
+		char* amountString = NULL;
+		amountString = formatNum(p->amount, p->isAdd);
 		
-		/*char* balanceString = NULL;
+		char* balanceString = NULL;
 		if(balance<0){
-			balanceString = buildAmount(-balance, 0);
+			balanceString = formatNum(-balance, 0);
 		}
 		else{
-			balanceString = buildAmount(balance, 1);
-		}*/
-		//char descString[25];
-		//strncpy(descString, p->description, sizeof(descString));
-		//descString[24] = '\0';
-		//strcat(line, descString);
+			balanceString = formatNum(balance, 1);
+		}
+		
+		char* descString = formatDesc(p->description);
+		strcat(line, descString);
 		strcat(line, " | ");
 		strcat(line, amountString);
-		//strcat(line, " | ");
-		//strcat(line, balanceString);
+		strcat(line, " | ");
+		strcat(line, balanceString);
 		strcat(line, " |");
-		//printf("%s\n", line);
-		printf("%s\t%d\t%d\n", dateString, p->amount, balance);
+		printf("%s\n", line);
 		cur = My402ListNext(trans, cur);
 
 	}
-	printf("%s", header3);
+	printf("%s\n", header3);
+	fclose(fp);
 }
 
-int main(){
+int main(int argc, char* argv[]){
 	sort("test.txt");
 	return 0;
 }
